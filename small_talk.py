9492e737c62a573f09486686b2c7f0e4a1a93df7
Mdfaizalsheikh <mdfaizal695@gmail.com>
2024-06-28 07:48:23 +0530

    Create small_talk.py

---
+++ small_talk.py
@@ -0,0 +1,8 @@
+import random
+def small_talks():
+  responses =[
+    "Hi! How can I assist you today?",
+        "Hello! What's on your mind?",
+        "Hey there! How can I help?"
+  ]
+  return random.choice(responses)
